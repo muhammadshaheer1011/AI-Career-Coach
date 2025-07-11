ea24004bc8164a52591d3ea42bcc293465d490cc
k228719 <k228719@nu.edu.pk>
2025-07-11 19:19:23 +0500

    Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol:'https',
-                hostname:'randomuser.me'
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+        pathname: '/api/portraits/**', // Allow all portrait paths
+      },
+    ],
+  },
+
+  // Good practice settings
+  reactStrictMode: true,
 };
 
 export default nextConfig;
